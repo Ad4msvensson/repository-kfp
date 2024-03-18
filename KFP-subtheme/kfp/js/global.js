67bc00cb913e59986cf5765183c4be69945e6970
Ad4msvensson <125870991+Ad4msvensson@users.noreply.github.com>
2024-03-18 13:36:56 +0100

    Add files via upload

---
+++ KFP-subtheme/kfp/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.kfp = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
